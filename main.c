20ecfd8da1b821d0f404a96876c2c63251e7d641
Thais Mina nishimoto <tmina-ni@c1r8p2.42sp.org.br>
2023-07-26 17:59:41 -0300

    initial tests

--- main.c
+++ main.c
@@ -6,13 +6,12 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 15:28:43 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/07/25 18:06:33 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/07/26 17:58:38 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <X11/X.h>
 #include <X11/keysym.h>
 #include <mlx.h>
@@ -23,12 +22,76 @@
 #define MLX_ERROR 1
 
 #define RED_PIXEL 0xFF0000
+#define GREEN_PIXEL 0xFF00
+#define BLUE_PIXEL 0xFF
+
+typedef struct s_img
+{
+    void	*mlx_img;
+    char	*addr;
+    int		bpp; /* bits per pixel */
+    int		line_len;
+    int		endian;
+}	t_img;
 
 typedef struct s_data
 {
     void	*mlx_ptr;
     void	*win_ptr;
+    t_img	img;
 }	t_data;
+
+typedef struct s_rect
+{
+    int	x;
+    int	y;
+    int width;
+    int height;
+    int color;
+}	t_rect;
+
+int render_rect(t_data *data, t_rect rect)
+{
+    int	i;
+    int j;
+
+    if (data->win_ptr == NULL)
+        return (1);
+    i = rect.y;
+    while (i < rect.y + rect.height)
+    {
+        j = rect.x;
+        while (j < rect.x + rect.width)
+            mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+        ++i;
+    }
+    return (0);
+}
+
+int render_backgroud(t_data *data, t_rect rect)
+{
+    int	i;
+    int j;
+
+    if (data->win_ptr == NULL)
+        return (1);
+    i = 0;
+    while (i < WINDOW_HEIGHT)
+    {
+        j = 0;
+        while (j < WINDOW_WIDTH)
+            mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+        ++i;
+    }
+    return (0);
+}
+
+int	render(t_data *data)
+{
+    render_rect(data, (t_rect){0, WINDOW_HEIGHT / 2, WINDOW_WIDTH, 1, GREEN_PIXEL});
+    render_backgroud(data, (t_rect){0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, BLUE_PIXEL});
+    return (0);
+}
 
 int	handle_keypress(int keysym, t_data *data)
 {
@@ -40,15 +103,6 @@ int	handle_keypress(int keysym, t_data *data)
     return (0);
 }
 
-int	render(t_data *data)
-{
-    /* if window has been destroyed, we don't want to put the pixel ! */
-    if (data->win_ptr != NULL)
-        mlx_pixel_put(data->mlx_ptr, data->win_ptr, 
-            WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, RED_PIXEL);
-    return (0);
-}
-
 int	main(void)
 {
     t_data	data;
@@ -56,17 +110,18 @@ int	main(void)
     data.mlx_ptr = mlx_init();
     if (data.mlx_ptr == NULL)
         return (MLX_ERROR);
-    data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
-                                "my window");
+    data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
     if (data.win_ptr == NULL)
     {
         free(data.win_ptr);
         return (MLX_ERROR);
     }
 
+    data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+
     /* Setup hooks */ 
     mlx_loop_hook(data.mlx_ptr, &render, &data);
-    mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+    mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data); /* ADDED */
 
     mlx_loop(data.mlx_ptr);
 
