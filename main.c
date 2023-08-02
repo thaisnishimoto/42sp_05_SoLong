ee3b601f06ed598a3e3fe9b337733b4def3a5b97
Thais Mina nishimoto <tmina-ni@c1r9p3.42sp.org.br>
2023-08-02 19:09:56 -0300

    render square on background

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 18:35:07 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/08/02 15:28:56 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/08/02 19:03:57 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,47 +20,49 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 	*(int *)pixel = color;
 }
 
-void	render_rect(t_img *img, int color)
+int	render_sqr(t_img *img, int color)
 {
 	int	x;
 	int	y;
 
-	x = WIN_WIDTH * 0.25;
-	while (x < WIN_WIDTH * 0.75)
+	x = 0;
+	while (x < 64)
 	{
-		y = WIN_HEIGHT * 0.25;
-		while (y < WIN_HEIGHT * 0.75)
+		y = 0;
+		while (y < 64)
 		{
 			img_pix_put(img, x, y, color);
 			y++;
 		}
 		x++;
 	}
-}
-
-int	render_next_frame(t_data *data)
-{
-	static int	i;
-
-	if (data->mlx_win == NULL)
-		return (1);
-	if (i >= 0 & i <= DELAY_FRAME * 5)
-		render_rect(&data->img, 0xFF0000);
-	if (i > DELAY_FRAME * 5 && i <= DELAY_FRAME * 10)
-		render_rect(&data->img, 0x00FF00);
-	if (i > DELAY_FRAME * 10 && i <= DELAY_FRAME * 15)
-		render_rect(&data->img, 0xFF);
-	if (i > DELAY_FRAME * 15)
-		i = 0;
-	mlx_put_image_to_window(data->mlx_connection, data->mlx_win, data->img.mlx_img, 0, 0);
-	i++;
 	return (0);
 }
+
+//int	move_sqr(t_data *data)
+//{
+//	static int	i;
+//
+//	if (data->mlx_win == NULL)
+//		return (1);
+//	if (i >= 0 & i <= DELAY_FRAME * 5)
+//		render_rect(&data->img, 0xFF0000);
+//	if (i > DELAY_FRAME * 5 && i <= DELAY_FRAME * 10)
+//		render_rect(&data->img, 0x00FF00);
+//	if (i > DELAY_FRAME * 10 && i <= DELAY_FRAME * 15)
+//		render_rect(&data->img, 0xFF);
+//	if (i > DELAY_FRAME * 15)
+//		i = 0;
+//	mlx_put_image_to_window(data->mlx_connection, data->mlx_win, data->img.mlx_img, 0, 0);
+//	i++;
+//	return (0);
+//}
 
 int	render_space(t_data *data)
 {
 	int	x;
 	int	y;
+	static int	i;
 
 	y = 0;
 	if (data->mlx_win != NULL)
@@ -70,12 +72,32 @@ int	render_space(t_data *data)
 			x = 0;
 			while (x * 64 < WIN_WIDTH)
 			{
-				mlx_put_image_to_window(data->mlx_connection, data->mlx_win, data->map.ptr, x * 64, y * 64);
+				if (i >= 0 & i <= DELAY_FRAME * 5)
+      					mlx_put_image_to_window(data->mlx_connection, data->mlx_win, data->space1.ptr, x * 64, y * 64);
+				if (i > DELAY_FRAME * 5 && i <= DELAY_FRAME * 10)
+					mlx_put_image_to_window(data->mlx_connection, data->mlx_win, data->space2.ptr, x * 64, y * 64);
+				if (i > DELAY_FRAME * 10 && i <= DELAY_FRAME * 15)
+					mlx_put_image_to_window(data->mlx_connection, data->mlx_win, data->space3.ptr, x * 64, y * 64);
+				if (i > DELAY_FRAME * 15 && i <= DELAY_FRAME * 20)
+					mlx_put_image_to_window(data->mlx_connection, data->mlx_win, data->space4.ptr, x * 64, y * 64);
+				if (i > DELAY_FRAME * 20)
+					i = 0;
+				i++;
 				x++;
 			}
 			y++;
 		}
     	}
+	return (0);
+}
+
+int     render_game(t_data *data)
+{
+	if (data->mlx_win == NULL)
+		return (1);
+	render_space(data);
+	render_sqr(&data->img, 0xFFFFFF);
+	mlx_put_image_to_window(data->mlx_connection, data->mlx_win, data->img.mlx_img, 0, 0);
 	return (0);
 }
 
@@ -86,13 +108,24 @@ int	x_win(t_data *data)
 	return (0);
 }
 
-int	esc_win(int keysym, t_data *data)
+int	key_hook(int keysym, t_data *data)
 {
 	if (keysym == XK_Escape)
 	{
         	mlx_destroy_window(data->mlx_connection, data->mlx_win);
         	data->mlx_win = NULL;
 	}
+	printf("Key pressed: %d/n", keysym);
+	return (0);
+}
+
+int	init_sprites(t_data* data)
+{
+	data->space1.ptr = mlx_xpm_file_to_image(data->mlx_connection, SPACE_PATH1, &data->space1.width, &data->space1.height);
+//	printf("size of frame1: %d x %d", data->space1.width, data->space1.height);
+	data->space2.ptr = mlx_xpm_file_to_image(data->mlx_connection, SPACE_PATH2, &data->space2.width, &data->space2.height);
+	data->space3.ptr = mlx_xpm_file_to_image(data->mlx_connection, SPACE_PATH3, &data->space3.width, &data->space3.height);
+	data->space4.ptr = mlx_xpm_file_to_image(data->mlx_connection, SPACE_PATH4, &data->space4.width, &data->space4.height);
 	return (0);
 }
 
@@ -100,22 +133,23 @@ int	main(void)
 {
 	t_data	data;
 
-	data.map.relative_path = "./texture_space.xpm";
 	data.mlx_connection = mlx_init();
 	data.mlx_win = mlx_new_window(data.mlx_connection, WIN_WIDTH, WIN_HEIGHT, "SO LONG");
-	data.map.ptr = mlx_xpm_file_to_image(data.mlx_connection, data.map.relative_path, &data.map.width, &data.map.height);
-	data.img.mlx_img = mlx_new_image(data.mlx_connection, WIN_WIDTH, WIN_HEIGHT);
+	init_sprites(&data);
+	data.img.mlx_img = mlx_new_image(data.mlx_connection, 64, 64);
 	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
 	/*Hooks*/
-//	mlx_loop_hook(data.mlx_connection, &render_space, &data); //background
-	mlx_loop_hook(data.mlx_connection, &render_next_frame, &data); // animation
-	mlx_hook(data.mlx_win, KeyPress, KeyPressMask, &esc_win, &data);
+	mlx_hook(data.mlx_win, KeyPress, KeyPressMask, &key_hook, &data);
 	mlx_hook(data.mlx_win, DestroyNotify, StructureNotifyMask, &x_win, &data);
+	mlx_loop_hook(data.mlx_connection, &render_game, &data);
 
 	mlx_loop(data.mlx_connection);
 	
-	mlx_destroy_image(data.mlx_connection, data.map.ptr);
-//	mlx_destroy_image(data.mlx_connection, data.img.mlx_img);
+	mlx_destroy_image(data.mlx_connection, data.space1.ptr);
+	mlx_destroy_image(data.mlx_connection, data.space2.ptr);
+	mlx_destroy_image(data.mlx_connection, data.space3.ptr);
+	mlx_destroy_image(data.mlx_connection, data.space4.ptr);
+	mlx_destroy_image(data.mlx_connection, data.img.mlx_img);
 	mlx_destroy_display(data.mlx_connection);
 	free(data.mlx_connection);
 	return (0);
