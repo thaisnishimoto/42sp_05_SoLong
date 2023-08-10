15822141fa5e2e616d5062b43d1c2cc8ec89fdea
Thais Nishimoto <thaismnishimoto@gmail.com>
2023-08-10 12:50:50 -0300

    player stops at walls

--- src/move_player.c
+++ src/move_player.c
@@ -6,48 +6,76 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 16:37:20 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/08/09 19:17:42 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/08/10 12:49:33 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+int	check_front_is_wall(t_data *game, int x, int y)
+{
+	if (game->astronaut.direction == 'u')
+		y--;
+	else if (game->astronaut.direction == 'd')
+		y++;
+	else if (game->astronaut.direction == 'r')
+		x++;
+	else if (game->astronaut.direction == 'l')
+		x--;
+	if (game->map.grid[y][x] == '1')
+		return (1);
+	else
+		return (0);
+}
+
 void	move_up(t_data *game, t_player *astronaut)
 {
-	game->map.grid[astronaut->y][astronaut->x] = '0';
-	astronaut->y--;
-	game->map.grid[astronaut->y][astronaut->x] = 'P';
 	astronaut->direction = 'u';
-	astronaut->move_count++;
-	ft_printf("Movement count = %d\n", astronaut->move_count);
+	if (check_front_is_wall(game, astronaut->x, astronaut->y) != 1)
+	{
+		game->map.grid[astronaut->y][astronaut->x] = '0';
+		astronaut->y--;
+		game->map.grid[astronaut->y][astronaut->x] = 'P';
+		astronaut->move_count++;
+		ft_printf("Movement count = %d\n", astronaut->move_count);
+	}
 }
 
 void	move_down(t_data *game, t_player *astronaut)
 {
-	game->map.grid[astronaut->y][astronaut->x] = '0';
-	astronaut->y++;
-	game->map.grid[astronaut->y][astronaut->x] = 'P';
 	astronaut->direction = 'd';
-	astronaut->move_count++;
-	ft_printf("Movement count = %d\n", astronaut->move_count);
+	if (check_front_is_wall(game, astronaut->x, astronaut->y) != 1)
+	{
+		game->map.grid[astronaut->y][astronaut->x] = '0';
+		astronaut->y++;
+		game->map.grid[astronaut->y][astronaut->x] = 'P';
+		astronaut->move_count++;
+		ft_printf("Movement count = %d\n", astronaut->move_count);
+	}
 }
 
 void	move_right(t_data *game, t_player *astronaut)
 {
-	game->map.grid[astronaut->y][astronaut->x] = '0';
-	astronaut->x++;
-	game->map.grid[astronaut->y][astronaut->x] = 'P';
 	astronaut->direction = 'r';
-	astronaut->move_count++;
-	ft_printf("Movement count = %d\n", astronaut->move_count);
+	if (check_front_is_wall(game, astronaut->x, astronaut->y) != 1)
+	{
+		game->map.grid[astronaut->y][astronaut->x] = '0';
+		astronaut->x++;
+		game->map.grid[astronaut->y][astronaut->x] = 'P';
+		astronaut->move_count++;
+		ft_printf("Movement count = %d\n", astronaut->move_count);
+	}
 }
 
 void	move_left(t_data *game, t_player *astronaut)
 {
-	game->map.grid[astronaut->y][astronaut->x] = '0';
-	astronaut->x--;
-	game->map.grid[astronaut->y][astronaut->x] = 'P';
 	astronaut->direction = 'l';
-	astronaut->move_count++;
-	ft_printf("Movement count = %d\n", astronaut->move_count);
+	if (check_front_is_wall(game, astronaut->x, astronaut->y) != 1)
+	{
+		game->map.grid[astronaut->y][astronaut->x] = '0';
+		astronaut->x--;
+		game->map.grid[astronaut->y][astronaut->x] = 'P';
+		astronaut->move_count++;
+		ft_printf("Movement count = %d\n", astronaut->move_count);
+	}
 }
